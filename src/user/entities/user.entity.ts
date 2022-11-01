b69dd122bef961e3bf0c5ad8233a77136acee18d
yashino <szczypiorpan123@gmail.com>
2022-11-01 17:37:13 +0100

    Created UserEntity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from '../../types/user/user-role';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    unique: true,
+  })
+  username: string;
+  @Column({
+    unique: true,
+  })
+  email: string;
+  @Column({
+    nullable: true,
+    default: null,
+  })
+  name: string | null;
+  @Column({
+    nullable: true,
+    default: null,
+  })
+  surname: string | null;
+  @Column({
+    length: 60,
+  })
+  hashedPassword: string;
+  @Column({
+    type: 'tinyint',
+    unsigned: true,
+    default: UserRole.User,
+  })
+  role: UserRole;
+}
