4ef6ad64b40eb6650c690f8202605f3376d30261
yashino <szczypiorpan123@gmail.com>
2022-11-01 18:38:37 +0100

    Added global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   const { PORT } = process.env;
   await app.listen(PORT);
 }
