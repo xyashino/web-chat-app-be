404218bbc90e3ab3371293a6142a5b5a62399267
yashino <szczypiorpan123@gmail.com>
2022-11-02 21:14:05 +0100

    The logout method has been unpainted and the phoneRegister method has been renamed to login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,25 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
 import { AuthLoginDto } from './dto/auth-login.dto';
-// import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/entities/user.entity';
+import { UserObj } from '../decorators/user-decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  async phoneRegister(
-    @Body() req: AuthLoginDto,
-    @Res() res: Response,
-  ): Promise<any> {
+  async login(@Body() req: AuthLoginDto, @Res() res: Response): Promise<any> {
     return this.authService.login(req, res);
   }
-  //
-  // @Get('/logout')
-  // @UseGuards(AuthGuard('jwt'))
-  // async logout(@UserObj() user: User, @Res() res: Response) {
-  //   return this.authService.logout(user, res);
-  // }
+
+  @Get('/logout')
+  @UseGuards(AuthGuard('jwt'))
+  async logout(@UserObj() user: User, @Res() res: Response) {
+    console.log(user);
+    debugger;
+    return this.authService.logout(user, res);
+  }
 }
