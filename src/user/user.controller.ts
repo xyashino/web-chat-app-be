d358fed9f13fc4f8bbc55cc4b5f53e1432b8ef3e
yashino <szczypiorpan123@gmail.com>
2022-11-03 17:34:46 +0100

    Added listening get and patch on user/current

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,6 +13,8 @@ import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { UserObj } from '../decorators/user-decorator';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -24,15 +26,28 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @Get('/current')
+  @UseGuards(AuthGuard('jwt'))
+  getCurrentUser(@UserObj() user: User) {
+    return this.userService.getCurrentUser(user);
+  }
+  @Patch('/current')
+  @UseGuards(AuthGuard('jwt'))
+  updateCurrentUser(@UserObj() user: User) {
+    return this.userService.updateCurrentUser(user);
+  }
+
   @Post('/register')
   register(@Body() body: RegisterUserDto) {
     return this.userService.register(body);
   }
+
   @UseGuards(AuthGuard('jwt'))
   @Get('/:id')
   findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.userService.findOne(id);
   }
+
   @UseGuards(AuthGuard('jwt'))
   @Patch('/:id')
   update(
@@ -41,6 +56,7 @@ export class UserController {
   ) {
     return this.userService.update(id, updateUserDto);
   }
+
   @UseGuards(AuthGuard('jwt'))
   @Delete('/:id')
   remove(@Param('id') id: string) {
